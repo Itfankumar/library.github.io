5704cdfbe1bf5977779c01987aff79328be82e12
Itfankumar <itachiuchiha261135@gmail.com>
2026-06-07 19:07:17 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,44 @@
+// Arithmetic Opretor
+
+// let a = 12;
+// let b = 6;
+
+// console.log("a + b = " ,  a + b);
+// console.log("a ** b = " ,  a ** b);
+
+// Unary Opretor
+
+// let a = 12;
+// let b = 6;
+
+// ++a ;
+// console.log("a = " , a);
+// console.log("a = ", a , "& b = ",
+
+
+
+
+//Asighment Opretor
+
+let a = 12;
+let b = 13;
+
+a /= 4;
+
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
